c57c8eac1b895aaa41f970d2a4b11adc1a585e05
JuanMaCarrearas <manuucarreras@gmail.com>
2022-12-05 12:04:12 -0300

    Modelos Terminados

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,8 +4,44 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+    id: {                                       //ID(3 digitos)
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false
+    },
+
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    flag: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    continet: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    subregion: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    area: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    poblation: {
+      type: DataTypes.INTEGER,
       allowNull: false,
     },
   });
