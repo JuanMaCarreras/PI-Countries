f449808f9be5d9273dd74d3975fcb2b442b7ef03
JuanMaCarrearas <manuucarreras@gmail.com>
2022-12-15 12:50:24 -0300

    Rutas Country Terminadas

---
+++ api/src/routes/controllers/coutriesController.js
@@ -0,0 +1,76 @@
+const { Country, Activity } = require('../../db')
+const { Op } = require("sequelize");
+
+const getAllCountries = async (req, res) => {
+
+    const { name } = req.query
+
+    try {
+
+        if (name) {
+
+            const queryCountry = await Country.findAll({
+                where: {
+                    name: {
+                        [Op.iLike]: `%${name}%`
+                    },
+                },
+                include: Activity
+            })
+
+
+            if (queryCountry.length === 0) {
+                return res.status(404).json({ error: `No country found with name, ${name}` })
+            }
+            return res.status(200).send(queryCountry)
+
+
+        } else {
+            const resCountry = await Country.findAll({ include: Activity })
+
+            res.send(resCountry)
+        }
+
+
+    } catch (error) {
+
+        res.send(error)
+
+    }
+
+};
+
+
+
+const getCountriesById = async (req, res) => {
+
+    const { id } = req.params
+
+    try {
+
+        const idCountry = await Country.findOne({
+            where: {
+                id,
+            },
+            include: Activity,
+        });
+
+        if (idCountry.length === 0) {
+            return res.status(404).json({ error: 'Country not found' })
+        }
+        return res.status(200).send(idCountry)
+
+
+    } catch (error) {
+
+        res.send(error)
+    }
+
+};
+
+
+
+module.exports = {
+    getAllCountries,
+    getCountriesById
+}
---
+++ api/src/routes/countryRouter.js
@@ -0,0 +1,11 @@
+const { Router } = require('express')
+const { getAllCountries, getCountriesById } = require('./controllers/coutriesController.js')
+
+const router = Router()
+
+
+router.get('/', getAllCountries)
+
+router.get('/:id', getCountriesById)
+
+module.exports = router;
