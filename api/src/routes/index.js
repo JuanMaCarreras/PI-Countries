c659e7e0c50beb06cda7a18888df7ed9dc1464f7
JuanMaCarrearas <manuucarreras@gmail.com>
2022-12-15 19:14:53 -0300

    Back Funcionando

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,22 @@
 const { Router } = require('express');
+const CountryRouter = require('./countryRouter.js');
+const ActivityRouter = require('./activityRouter.js')
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
 
+
+
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.use('/countries', CountryRouter)
+
+router.use('/activities', ActivityRouter)
+
 
 
 module.exports = router;
