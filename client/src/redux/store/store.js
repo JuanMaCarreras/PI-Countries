e8ef2e362562e3ab72e70e723aebd71486adcc00
JuanMaCarrearas <manuucarreras@gmail.com>
2022-12-21 12:44:09 -0300

    Store Terminado

---
+++ client/src/redux/store/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import rootReducer from '../reducer'
+import thunkMiddleware from "redux-thunk";
+
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // redux devTools chrome extencion
+
+const store = createStore(
+    rootReducer,
+    composeEnhancer(applyMiddleware(thunkMiddleware))
+);
+
+
+module.exports = store;
