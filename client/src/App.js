72e85f0805124df92c60d345f8a84f963bb293eb
JuanMaCarrearas <manuucarreras@gmail.com>
2022-12-28 12:09:38 -0300

    Rutas Casi Terminadas

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { Route } from 'react-router-dom'
+import Landing from './components/Landing/Landing.jsx';
+import Home from './components/Home/Home'
+import NavBar from './components/NavBar/NavBar';
+import Form from './components/Form/Form'
+//pruba
+import CardDetail from './components/CardDetail/CardDetail';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Countries</h1>
+
+      <Route exact path={'/'} component={Landing} />
+      <Route component={NavBar} />
+      <Route path={'/home'} component={Home} />
+      <Route path={'/card/detail/:id'} component={CardDetail} />
+      <Route path={'/home/create'} component={Form} />
+
     </div>
   );
 }
