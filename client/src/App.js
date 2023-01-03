6f1c04aee73125267f4969cafd49f62dda654cb3
JuanMaCarrearas <manuucarreras@gmail.com>
2023-01-03 17:24:02 -0300

    Rutas del Front Terminadas

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,19 @@ import './App.css';
 import { Route } from 'react-router-dom'
 import Landing from './components/Landing/Landing.jsx';
 import Home from './components/Home/Home'
-import NavBar from './components/NavBar/NavBar';
 import Form from './components/Form/Form'
 import CardDetail from './components/CardDetail/CardDetail';
 
-
-
 function App() {
+
+
   return (
     <div className="App">
 
       <Route exact path={'/'} component={Landing} />
-      <Route component={NavBar} />
-      <Route path={'/home'} component={Home} />
-      <Route path={'/card/detail/:id'} component={CardDetail} />
-      <Route path={'/home/create'} component={Form} />
+      <Route exact path={'/home'} component={Home} />
+      <Route exact path={'/countries/:id'} component={CardDetail} />
+      <Route exact path={'/home/create'} component={Form} />
 
     </div>
   );
