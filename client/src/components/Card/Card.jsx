15716474d00a8165ae08307b4ff09f039804f0b9
JuanMaCarrearas <manuucarreras@gmail.com>
2023-01-05 12:47:27 -0300

    Card Terminada

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -3,18 +3,17 @@ import style from './Card.module.css'
 import { Link } from 'react-router-dom'
 
 
-function Card({ flag, name, continent, id }) {
+function Card({ flagimg, name, continent, id }) {
     return (
         <>
-            <Link to={`/countries/detail/${id}`}>
+            <Link className={style.link} to={`/countries/${id}`}>
                 <div className={style.cardContainer}>
-                    <div className={style.cardImg}>
-                        <img src={flag} alt='img not found'></img>
-                    </div>
 
-                    <h2 className={style.cardName}>Name: {name}</h2>
+                    <img src={flagimg} alt='img not found'></img>
 
-                    <h3 className={style.cardContinent}>Continent: {continent}</h3>
+                    <h2 className={style.cardName}>{name}</h2>
+
+                    <h4 className={style.cardContinent}>{continent}</h4>
 
                 </div>
             </Link>
