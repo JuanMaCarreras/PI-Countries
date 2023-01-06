b6ec27200205f6308c835ab81e2549de514fb246
JuanMaCarrearas <manuucarreras@gmail.com>
2023-01-05 21:03:38 -0300

    CardDetail Terminada

--- client/src/components/CardDetail/CardDetail.jsx
+++ client/src/components/CardDetail/CardDetail.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import { getDetails } from '../../redux/actions/index'
 import style from './CardDetail.module.css'
 import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 function CardDetail() {
@@ -30,24 +31,48 @@ function CardDetail() {
                     {
                         details ? < div >
 
-                            <div className={style.flagContainer}>
+                            <Link to='/home'>
+                                <button className={style.btn}>
+                                    <ion-icon name="globe-outline" />
+                                </button>
+                            </Link>
+
+                            <h1 className={style.name}> <span> Name: </span> {details.name}</h1>
+
+                            <div className={style.Container}>
                                 <img src={details.flag} alt="img not found" />
+
+                                <h3 className={style.data} > {details.id}</h3>
+
+                                <h4 className={style.data}> <span> Capital: </span> {details.capital}</h4>
+                                <h4 className={style.data}> <span> Continent: </span> {details.continent}</h4>
+                                <h4 className={style.data}> <span> Subregion: </span> {details.subregion}</h4>
+                                <h4 className={style.data}> <span> Area: </span> {details.area} km2</h4>
+                                <h4 className={style.data}>  <span>Poblation: </span> {details.poblation}</h4>
                             </div>
-
-                            <h1 className={style.name} > Name: {details.name}</h1>
-                            <h3 className={style.id} > {details.id}</h3>
-
-                            <h5 className={style.Cont}> Continent: {details.continent}</h5>
-                            <h5 className={style.Cap}> Capital: {details.capital}</h5>
-                            <h5 className={style.subregion}> Subregion: {details.subregion}</h5>
-                            <h5 className={style.area}> Area: {details.area} km2</h5>
-                            <h5 className={style.poblation}> Poblation: {details.poblation} km2</h5>
                         </div >
                             : <h2 className={style.Loading}>Loading...</h2>
 
                     }
 
+                    {
+                        details ?
+                            details.activities[0] ?
+                                <div className={style.containerActivity}>
+                                    <h4 className={style.dataAct}> Activity: {details.activities[0].name} </h4>
+                                    <h4 className={style.dataAct}> Difficulty: {details.activities[0].difficulty} </h4>
+                                    <h4 className={style.dataAct}> Duration: {details.activities[0].duration} horas</h4>
+                                    <h4 className={style.dataAct}> Season: {details.activities[0].season} </h4>
+                                </div>
+
+                                :
+                                <h3 className={style.noActivity}>Activities not Found</h3>
+                            :
+                            <h2>Loading...</h2>
+
+                    }
                 </div>
+
             </div>
 
 
