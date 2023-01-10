a6ee99974b6e28dc3c0c3918a6d3cebe3b87df77
JuanMaCarrearas <manuucarreras@gmail.com>
2023-01-10 17:03:31 -0300

    Nuevo Paginado

--- client/src/components/Paginado/Paginado.jsx
+++ client/src/components/Paginado/Paginado.jsx
@@ -1,7 +1,7 @@
 import style from './Paginado.module.css'
 
 
-function Paginado({ countriesPerPage, allCountries, paginado, currentPage }) {
+function Paginado({ countriesPerPage, allCountries, paginado, currentPage, firstPage }) {
 
     const numPage = []
 
@@ -13,11 +13,29 @@ function Paginado({ countriesPerPage, allCountries, paginado, currentPage }) {
     return (
         <>
             <div className={style.container}>
-                {
+
+
+                {/* {
                     numPage.map(p => <button className={currentPage === p ? style.btn2 : style.btnPag}
                         key={p}
                         onClick={() => paginado(p)} >{p}</button>)
-                }
+                } */}
+
+                <div className={style.btnPag}
+                    onClick={() => {
+                        if (currentPage > 1)
+                            paginado(currentPage - 1)
+                    }} >{<ion-icon name='chevron-back-outline' />}</div>
+
+                <div className={style.btnPag} >{currentPage}</div>
+
+                <div className={style.btnPag}
+                    onClick={() => {
+                        if (currentPage < allCountries) paginado(currentPage + 1)
+                    }}
+                >
+                    <ion-icon name='chevron-forward-outline' />
+                </div>
 
             </div>
         </>
